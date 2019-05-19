5845c2c9ef83b1af8b8a64435d258425617c37a1
scaraclette <gustiscarlett@gmail.com>
2019-05-19 13:06:43 -0700

    updated method

--- flask-method/application.py
+++ flask-method/application.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-currentInput = None
+currentInput = ""
 
 @app.route("/")
 def index():
@@ -17,16 +17,18 @@ def call_function():
     # To get the user input, Flask requests the input field from the HTML's name attribute. A past mistake I've done was using an id attribute instead of a name attribue.
     # Request.form.get returns None if the methods are not specified. 
     input1 = request.form.get("userInput")  
-    print(input1)
     if input1 is not None:
         currentInput = input1.split()
+        print(type(currentInput)) 
         # Since this method does not rely on JavaScript, Python will handle the case of user submitting whitespace
         if len(currentInput) == 0:
-            currentInput = None
-        return render_template("input1.html", message=currentInput)    
+            currentInput = "no input submitted"
+        return render_template("input1.html", message=currentInput) 
+
+    print(type(input1))   
 
     # If input1 has no value, renders a page with message
-    return render_template("input1.html", message=currentInput)
+    return render_template("input1.html", message="no input submitted")
 
 
     
